05f958be9e502afb0f35a4b7c204a374ccb88b85
tangleihome <2697863232@qq.com>
2022-11-17 17:59:24 +0800

    壳子0.4

--- src/pages/UserContent.vue
+++ src/pages/UserContent.vue
@@ -48,7 +48,9 @@
           </ul>
           <div class="rb-more">more</div>
         </div>
-
+        <div class="map-box">
+          <div id="container" style="height: calc(100% - 48px);width:calc(100% - 74px);"></div>
+        </div>
         <div>
 
         </div>
@@ -60,11 +62,11 @@
             <span></span><span>Calendar</span>
           </div>
           <ul class="calendar-header">
-            <li v-for="(i,index) in 7" :key="index">{{ userDate.week[i-1] }}</li>
+            <li v-for="(i,index) in 7" :key="index">{{ userDate.week[i - 1] }}</li>
           </ul>
           <ul class="calendar-body">
             <li v-for="(number,index) in 35" :key="index" ref="calendarBodyNumber">
-              {{number - userDate.calendarOneDay > 0 && number - userDate.calendarOneDay<= userDate.monthNumber? number - userDate.calendarOneDay : ''}}
+              {{ number - userDate.calendarOneDay > 0 && number - userDate.calendarOneDay <= userDate.monthNumber ? number - userDate.calendarOneDay : '' }}
             </li>
           </ul>
         </div>
@@ -85,14 +87,17 @@
             </li>
           </ul>
         </div>
+
+
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import {reactive, onMounted,ref} from "vue";
+import {reactive, onMounted, ref} from "vue";
 import Axios from 'axios';
+import AMapLoader from '@amap/amap-jsapi-loader'
 
 Axios.defaults.baseURL = '/dingzhi'
 export default {
@@ -106,34 +111,68 @@ export default {
       date: ['', ''],
       week: ['日', '一', '二', '三', '四', '五', '六'],
       city: 101270101,
-      weatherImgSrc:require('@/assets/img/111.jpg'),
-      calendarOneDay:0,
-      monthNumber:30
+      weatherImgSrc: require('@/assets/img/111.jpg'),
+      calendarOneDay: 0,
+      monthNumber: 30
+    })
+
+    AMapLoader.load({
+      "key": "cf818aba226d51af0ea4ca8428cb59f9",          // 申请好的Web端开发者Key，首次调用 load 时必填
+      "version": "1.4.15",                                // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
+      "plugins": [],                                      // 需要使用的的插件列表，如比例尺'AMap.Scale'等
+      "AMapUI": {                                         // 是否加载 AMapUI，缺省不加载
+        "version": '1.1',                               // AMapUI 缺省 1.1
+        "plugins": [],                                   // 需要加载的 AMapUI ui插件
+      },
+      "Loca": {                                            // 是否加载 Loca， 缺省不加载
+        "version": '1.3.2'                              // Loca 版本，缺省 1.3.2
+      },
+    }).then((AMap) => {
+      var satellite = new AMap.TileLayer.Satellite();
+      var roadNet = new AMap.TileLayer.RoadNet();
+      var map = new AMap.Map('container', {
+        zoom: 110,                                        //级别
+        center: [103.890319, 30.788819],                 //中心点坐标
+        viewMode: '3D',      //使用3D视图
+        layers: [
+          satellite,
+          roadNet
+        ]
+      });
+      let marker = new AMap.Marker({
+        position: new AMap.LngLat(103.890319, 30.788819),
+        title: '我家'
+      })
+      map.add(marker)
+    }).catch(e => {
+      console.log('错误信息：', e);
 
     })
+
+
     onMounted(() => {
       const date = new Date()
       const month = date.getMonth()
       const year = date.getFullYear()
       const today = date.getDate()
-      userDate.calendarOneDay = new Date(`${year}-${month+1}-01`).getDate()+1
+      userDate.calendarOneDay = new Date(`${year}-${month + 1}-01`).getDate() + 1
       // 月份大小判断
-      if (month === 0||month === 2 || month === 4 || month === 6 || month=== 7||month===9||month===11){
+      if (month === 0 || month === 2 || month === 4 || month === 6 || month === 7 || month === 9 || month === 11) {
         userDate.monthNuber = 31
-      }else if (month===1){
+      } else if (month === 1) {
         userDate.monthNuber = 28
-      }else {
+      } else {
         userDate.monthNuber = 30
       }
 
       // 标记今天
-      for (var i =0;i<35;i++){
-        if (i - userDate.calendarOneDay < 0 || i - userDate.calendarOneDay>= userDate.monthNumber){
+      for (var i = 0; i < 35; i++) {
+        if (i - userDate.calendarOneDay < 0 || i - userDate.calendarOneDay >= userDate.monthNumber) {
           calendarBodyNumber.value[i].style.background = 'rgba(188,185,199,0.3)'
         }
       }
-      calendarBodyNumber.value[today+1].style.color = 'white'
-      calendarBodyNumber.value[today+1].style.background = '#5c72f6'
+      calendarBodyNumber.value[today + 1].style.color = 'white'
+      calendarBodyNumber.value[today + 1].style.background = '#5c72f6'
 
       userDate.date[0] = userDate.week[date.getDay()]
       userDate.date[1] = date.toLocaleDateString()
@@ -157,7 +196,7 @@ export default {
     })
     return {
       calendarBodyNumber,
-      userDate
+      userDate,
     }
   }
 }
@@ -190,6 +229,7 @@ export default {
 }
 
 .user-card {
+  width: 100%;
   border-radius: 8px;
   box-sizing: border-box;
   padding-top: 37px;
@@ -251,9 +291,11 @@ export default {
   box-sizing: border-box;
   padding: 24px 0;
   width: 100%;
+  height: 360px;
   margin-top: 24px;
   background: white;
   box-shadow: 0 0 10px 10px rgba(188, 185, 199, 0.1);
+  border-radius: 8px;
 }
 
 .rb-message ul {
@@ -299,7 +341,7 @@ export default {
   padding: 8px 24px 8px 24px;
 }
 
-.rb-more{
+.rb-more {
   box-sizing: border-box;
   width: 100%;
   height: 20px;
@@ -308,7 +350,15 @@ export default {
   font-size: 20px;
   text-align: right;
   padding-right: 37px;
+  cursor: pointer;
+  transition: 0.5s;
 }
+
+.rb-more:hover {
+  color: black;
+}
+
+
 .weather-img {
   height: 120px;
   width: 120px;
@@ -318,12 +368,28 @@ export default {
   border-radius: 16px;
   overflow: hidden;
 }
-.weather-img img{
+
+.weather-img img {
   position: absolute;
   top: -10px;
   left: -10px;
   height: 140px;
   width: 140px;
+}
+
+/*地图相关*/
+.map-box {
+  width: 100%;
+  overflow: hidden;
+  background: white;
+  margin-top: 24px;
+  border-radius: 8px;
+  height: calc(100% - 608px);
+}
+
+#container {
+  margin: 24px 37px;
+  border-radius: 4px;
 }
 
 .simple-todo-box {
@@ -335,30 +401,32 @@ export default {
   box-shadow: 0 0 10px 10px rgba(188, 185, 199, 0.1)
 }
 
-.calendar-title{
+.calendar-title {
   display: flex;
   height: 37px;
   width: calc(100% - 74px);
   margin-left: 37px;
   margin-top: 24px;
 }
-.calendar-title span{
+
+.calendar-title span {
   display: block;
 }
 
-.calendar-title span:first-child{
+.calendar-title span:first-child {
   height: 100%;
   width: 16px;
   background: #6adcc6;
 }
-.calendar-title span:last-child{
+
+.calendar-title span:last-child {
   font-size: 24px;
   font-weight: bold;
   text-indent: 8px;
   line-height: 37px;
 }
 
-.calendar-header{
+.calendar-header {
   display: flex;
   justify-content: space-between;
   list-style: none;
@@ -367,7 +435,8 @@ export default {
   margin-left: 37px;
   margin-top: 24px;
 }
-.calendar-header li{
+
+.calendar-header li {
   line-height: 37px;
   width: 37px;
   font-weight: bold;
@@ -375,7 +444,7 @@ export default {
 }
 
 /*日历本体*/
-.calendar-body{
+.calendar-body {
   box-sizing: border-box;
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
@@ -387,7 +456,7 @@ export default {
   padding-top: 12px;
 }
 
-.calendar-body li{
+.calendar-body li {
   line-height: 37px;
   width: 37px;
   margin: 8px;
@@ -400,12 +469,13 @@ export default {
   /*font-weight: bold;*/
 }
 
-.calendar-body li:hover{
+.calendar-body li:hover {
   box-shadow: 0 0 20px 2px rgba(85, 222, 195, 0.8);
   font-size: 20px;
 }
+
 /*待办样式*/
-.todo-lists{
+.todo-lists {
   list-style: none;
   text-align: left;
   margin-left: 37px;
@@ -414,19 +484,19 @@ export default {
   border-top: 1px solid #bcb9c7;
 }
 
-.todo-lists li{
+.todo-lists li {
   border-bottom: 1px solid #bcb9c7;
   height: 74px;
   line-height: 74px;
 
 }
 
-.todo-lists-box{
+.todo-lists-box {
   border-top: 4px rgba(188, 185, 199, 0.2) dashed;
   margin-top: 24px;
 }
 
-.lists-title{
+.lists-title {
   position: relative;
   display: flex;
   height: 37px;
@@ -434,23 +504,25 @@ export default {
   margin-left: 37px;
   margin-top: 24px;
 }
-.lists-title span{
+
+.lists-title span {
   display: block;
 }
 
-.lists-title span:first-child{
+.lists-title span:first-child {
   height: 100%;
   width: 16px;
   background: #6adcc6;
 }
 
-.todo-title-name{
+.todo-title-name {
   font-size: 24px;
   font-weight: bold;
   text-indent: 8px;
   line-height: 37px;
 }
-.lists-title div{
+
+.lists-title div {
   position: absolute;
   right: 24px;
   font-size: 24px;
@@ -464,12 +536,12 @@ export default {
   cursor: pointer;
 }
 
-.lists-title div:hover{
-    box-shadow: 0 0 18px 2px rgba(0, 157, 255, 0.8);
+.lists-title div:hover {
+  box-shadow: 0 0 18px 2px rgba(0, 157, 255, 0.8);
   font-size: 20px;
 }
 
-.todo-lists li{
+.todo-lists li {
   width: 100%;
   display: flex;
   align-items: center;
@@ -477,28 +549,29 @@ export default {
   justify-content: space-between;
 }
 
-.todo-lists input{
+.todo-lists input {
   height: 24px;
   width: 24px;
   /*flex: 1;*/
 }
 
-.todo-list-text{
+.todo-list-text {
   flex: 2;
   font-weight: bold;
   font-size: 20px;
   margin-left: 8px;
 }
 
-.todo-list-more{
+.todo-list-more {
   color: rgba(0, 0, 0, 0.5);
   cursor: pointer;
 }
 
-.todo-list-more span{
+.todo-list-more span {
   margin: 0 8px;
 }
-.todo-list-more span:first-child{
+
+.todo-list-more span:first-child {
   color: rgba(255, 0, 0, 0.5);
 }
 </style>
